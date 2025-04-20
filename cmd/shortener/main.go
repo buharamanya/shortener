d0d94d532aa499e12895c64e79f93534aef83850
tarakhmatullin <tarakhmatullin@domclick.ru>
2025-04-21 01:33:57 +0300

    increment 1 light server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,86 @@
 package main
 
-func main() {}
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"io"
+	"net/http"
+	"strings"
+)
+
+var urlStorage = make(map[string]string)
+
+const baseUrl string = "http://localhost:8080/"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", shortenURL)
+	mux.HandleFunc("/{shortCode}", redirectByShortUrl)
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func shortenURL(w http.ResponseWriter, r *http.Request) {
+	// проверяем метод запроса
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	// читаем тело запроса
+	originalURL, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	urlStr := strings.TrimSpace(string(originalURL))
+
+	// проверяем что URL не пустой
+	if urlStr == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("URL cannot be empty"))
+		return
+	}
+
+	// генерируем короткий код
+	hash := sha256.Sum256([]byte(urlStr))
+	shortCode := base64.URLEncoding.EncodeToString(hash[:6])
+	shortCode = strings.TrimRight(shortCode, "=")
+
+	// сохраняем в хранилище
+	shortURL := baseUrl + shortCode
+	urlStorage[shortCode] = urlStr
+
+	// возвращаем ответ
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(shortURL))
+}
+
+func redirectByShortUrl(w http.ResponseWriter, r *http.Request) {
+	// проверяем метод запроса
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	shortCode := strings.TrimPrefix(r.URL.Path, "/")
+
+	if shortCode == "" {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	originalURL, ok := urlStorage[shortCode]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Location", originalURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/buharamanya/shortener
+
+go 1.23.5
