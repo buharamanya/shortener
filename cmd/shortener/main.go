7f6f1a4fba618703ec5718ff81ddf87adc40250a
tarakhmatullin <tarakhmatullin@domclick.ru>
2025-04-26 23:51:48 +0300

    iter3: add chi router

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,17 +5,23 @@ import (
 
 	"github.com/buharamanya/shortener/internal/app/handlers"
 	"github.com/buharamanya/shortener/internal/app/storage"
+	"github.com/go-chi/chi/v5"
 )
 
 const baseURL string = "http://localhost:8080/"
 
 func main() {
 	repo := storage.NewInMemoryStorage()
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handlers.NewShortenHandler(repo, baseURL).ShortenURL)
-	mux.HandleFunc("/{shortCode}", handlers.NewRedirectHandler(repo).RedirectByShortURL)
-	err := http.ListenAndServe(`:8080`, mux)
-	if err != nil {
-		panic(err)
-	}
+	r := chi.NewRouter()
+	r.Post("/", handlers.NewShortenHandler(repo, baseURL).ShortenURL)
+	r.Get("/{shortCode}", handlers.NewRedirectHandler(repo).RedirectByShortURL)
+	// r передаётся как http.Handler
+	http.ListenAndServe(":8080", r)
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("/", handlers.NewShortenHandler(repo, baseURL).ShortenURL)
+	// mux.HandleFunc("/{shortCode}", handlers.NewRedirectHandler(repo).RedirectByShortURL)
+	// err := http.ListenAndServe(`:8080`, mux)
+	// if err != nil {
+	// 	panic(err)
+	// }
 }
