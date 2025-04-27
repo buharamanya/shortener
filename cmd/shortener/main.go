f5ac6a36896a9cba5f3598dba93211d1dc5e0ed4
tarakhmatullin <tarakhmatullin@domclick.ru>
2025-04-27 21:43:48 +0300

    iter4: add flags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,25 +3,26 @@ package main
 import (
 	"net/http"
 
+	"github.com/buharamanya/shortener/internal/app/config"
 	"github.com/buharamanya/shortener/internal/app/handlers"
 	"github.com/buharamanya/shortener/internal/app/storage"
 	"github.com/go-chi/chi/v5"
 )
 
-const baseURL string = "http://localhost:8080/"
+// const baseURL string = "http://localhost:8080/"
 
 func main() {
+	config.InitConfiguration()
+
 	repo := storage.NewInMemoryStorage()
+
 	r := chi.NewRouter()
-	r.Post("/", handlers.NewShortenHandler(repo, baseURL).ShortenURL)
+	r.Post("/", handlers.NewShortenHandler(repo, config.Config.RedirectBaseURL).ShortenURL)
 	r.Get("/{shortCode}", handlers.NewRedirectHandler(repo).RedirectByShortURL)
-	// r передаётся как http.Handler
-	http.ListenAndServe(":8080", r)
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/", handlers.NewShortenHandler(repo, baseURL).ShortenURL)
-	// mux.HandleFunc("/{shortCode}", handlers.NewRedirectHandler(repo).RedirectByShortURL)
-	// err := http.ListenAndServe(`:8080`, mux)
-	// if err != nil {
-	// 	panic(err)
-	// }
+
+	err := http.ListenAndServe(config.Config.ServerBaseURL, r)
+
+	if err != nil {
+		panic(err)
+	}
 }
