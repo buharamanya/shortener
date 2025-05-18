80c104ee68e43a094f5a05324bb772605e5a167f
tarakhmatullin <tarakhmatullin@domclick.ru>
2025-05-18 19:38:21 +0300

    iter6: logging

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,21 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"yandex-go/shortener/internal/app/logger"
 
 	"github.com/buharamanya/shortener/internal/app/config"
 	"github.com/buharamanya/shortener/internal/app/handlers"
+	"github.com/buharamanya/shortener/internal/app/logger"
 	"github.com/buharamanya/shortener/internal/app/storage"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	logger.Initialize("info")
+
 	var appConfig = config.InitConfiguration()
 
 	repo := storage.NewInMemoryStorage()
 
 	r := chi.NewRouter()
-	r.Post("/", handlers.NewShortenHandler(repo, appConfig.RedirectBaseURL).ShortenURL)
-	r.Get("/{shortCode}", handlers.NewRedirectHandler(repo).RedirectByShortURL)
+	r.Post(
+		"/",
+		logger.WithLogging(handlers.NewShortenHandler(repo, appConfig.RedirectBaseURL).ShortenURL),
+	)
+	r.Get(
+		"/{shortCode}",
+		logger.WithLogging(handlers.NewRedirectHandler(repo).RedirectByShortURL),
+	)
 
 	err := http.ListenAndServe(appConfig.ServerBaseURL, r)
 
